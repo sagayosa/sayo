b904df29b2fb45448c133047644245d692a75d3e
sagayosa <gyjgrteen@gmail.com>
2024-01-31 14:19:49 +0800

    improve CallCoreToPullCenter

--- pkg/job/pull.go
+++ pkg/job/pull.go
@@ -2,13 +2,15 @@ package job
 
 import (
 	servicecontext "sayo_framework/pkg/service_context"
+	"time"
 
 	"github.com/grteen/sayo_utils/module"
 	sayoerror "github.com/grteen/sayo_utils/sayo_error"
 	sayoinnerhttp "github.com/grteen/sayo_utils/sayo_inner_http"
+	sayolog "github.com/grteen/sayo_utils/sayo_log"
 )
 
-func CallCoreToPullCenter(svc *servicecontext.ServiceContext) error {
+func callCoreToPullCenter(svc *servicecontext.ServiceContext) error {
 	modules := svc.ModuleCenter.GetModulesByRole(module.RoleCore)
 	if len(modules) == 0 {
 		return sayoerror.Msg(sayoerror.ErrCallCoreToPullCenterFailed, "%v", "There are no core modules available")
@@ -19,4 +21,24 @@ func CallCoreToPullCenter(svc *servicecontext.ServiceContext) error {
 	}
 
 	return nil
+}
+
+// try callCoreToPullCenter until success
+func CallCoreToPullCenter(svc *servicecontext.ServiceContext) {
+	f := func() {
+		for {
+			time.Sleep(5 * time.Second)
+			if err := callCoreToPullCenter(svc); err != nil {
+				sayolog.Err(err).Error()
+				continue
+			}
+
+			return
+		}
+	}
+
+	go func() {
+		f()
+		sayolog.Msg("%v", "core successfully pulled the module center")
+	}()
 }
