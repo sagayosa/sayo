de94c38e5a5aa3c0a5ba3057ea1a9059da560b5d
sagayosa <gyjgrteen@gmail.com>
2024-02-13 15:43:54 +0800

    minor change

--- service/module/server.go
+++ service/module/server.go
@@ -8,11 +8,21 @@ import (
 type ModuleServer struct {
 	ctx context.Context
 	svc *servicecontext.ServiceContext
+	// registerLimiter chan struct{}
 }
+
+// func (s *ModuleServer) registerAccess() {
+// 	s.registerLimiter <- struct{}{}
+// }
+
+// func (s *ModuleServer) registerLeft() {
+// 	<-s.registerLimiter
+// }
 
 func NewModuleServer(ctx context.Context, svc *servicecontext.ServiceContext) *ModuleServer {
 	return &ModuleServer{
 		ctx: ctx,
 		svc: svc,
+		// registerLimiter: make(chan struct{}, 1),
 	}
 }
